55310a22bda3dfc35a5c4ce27598c68120c3630c
Florian Eckerstorfer <florian@theroadtojoy.at>
2012-02-09 23:46:05 +0100

    Layout, homepage.

---
+++ web/index.php
@@ -0,0 +1,75 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Florian Eckerstorfer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Florian Eckerstorfer is an Austrian Computer Science student and Web Developer. This is his private website.">
+	<meta name="author" content="Florian Eckerstorfer">
+
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link href="/css/master.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div id="header" class="container">
+	<div class="row">
+		<header class="span8">
+			<h1>Hello, I'm<br><strong>Florian<br>Eckerstorfer</strong>.</h1>
+		</header>
+		<aside class="span4">
+			<img src="/img/florian-eckerstorfer.jpg" alt="Photo of Florian Eckerstorfer" width="300" height="300">
+		</aside>
+	</div>
+</div>
+
+<div id="content" class="container">
+	<div class="row">
+		<div class="span8">
+			<p>I study <strong>Software Engineering &amp; Internet Computing</strong> and <strong>Innovation</strong> and work as a <strong>Web Developer</strong> in <strong>Vienna</strong>. My weblog is <a href="http://webadventures.at" title="WEBADVENTURES">WEBADVENTURES</a> and my portfolio is <a href="http://theroadtojoy.at">TRTJ</a>.</p>
+			<p>You also might be interested in my <a href="/bio">bio</a> or <a href="/cv">CV</a>.</p>
+		</div>
+		<div class="span4"></div>
+	</div>
+</div>
+
+<div id="social" class="container">
+	<div class="row">
+		<div class="span8">
+			<p><small>I am registered at numerous Social Networks, including <a href="http://twitter.com/Florian_">Twitter</a>, <a href="http://facebook.com/FlorianEckerstorfer">Facebook</a>, <a href="http://statigr.am/florian_">Instagram</a>, <a href="http://flickr.com/photos/florianeckerstorfer">Flickr</a>, <a href="http://last.fm/user/feredir">Last.fm</a>, <a href="http://gomiso.com/u/florian--2">Miso</a> and <a href="https://github.com/florianeckerstorfer">Github</a>.</small></p></div>
+	</div>
+</div>
+
+<div id="share" class="container">
+	<div class="row">
+		<div class="span8">
+			<div class="fb-like" data-href="http://florianeckerstorfer.com" data-send="true" data-width="450" data-show-faces="true"></div>&nbsp;
+		</div>
+		<div class="social-button span4">
+			<a href="https://twitter.com/Florian" class="twitter-follow-button" data-show-count="false" data-width="109px">Follow @Florian_</a>
+		</div>
+	</div>
+</div>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+</body>
+</html>
